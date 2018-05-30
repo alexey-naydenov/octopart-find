15689d8420986abb5ad077f4f8cfe4e58c32fc3c
Alexey Naydenov <dev@alexeynaydenov.triacontahedron.com>
2018-05-30 18:20:22 +0300

    Create empty package file

---
+++ octopart-find.el
@@ -0,0 +1,31 @@
+;;; octopart-find.el --- Electronic components lookup  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Alexey Naydenov
+
+;; Author: Alexey Naydenov <dev@alexeynaydenov.triacontahedron.com>
+;; Keywords: tools, comm
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'octopart-find)
+;;; octopart-find.el ends here
